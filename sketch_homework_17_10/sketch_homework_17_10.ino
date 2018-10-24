c43257ec40da90922e52a8b9ca62fac45d8a835e
Quan <urjhams@gmail.com>
2018-10-24 21:17:46 +0200

    update

--- sketch_homework_17_10/sketch_homework_17_10.ino
+++ sketch_homework_17_10/sketch_homework_17_10.ino
@@ -3,12 +3,31 @@
 
 Adafruit_Microbit_Matrix microbit;
 
+int drop_column = random(5);
+// player always be initialized in the middle column (index 2)
+int player_column = 2;
+
+// the ball at first start to drop at row 1 (index 0)
+int droping_at_row = 0;
+
+enum cases {
+  empty = B00000,
+  first = B10000,
+  second = B01000,
+  third = B00100,
+  fourth = B00010,
+  fifth = B00001
+};
+
+const uint8_t ledCases[] = {first, second, third, fourth, fifth};
+
 OneButton buttonLeft(5, true);
 OneButton buttonRight(11, true);
 
 void setup() {
-  buttonLeft.attachSingleClick(clickLeft);
-  buttonRight.attachSingleClick(clickRight);
+  microbit.begin();
+  buttonLeft.attachClick(clickLeft);
+  buttonRight.attachClick(clickRight);
 }
 
 void loop() {
@@ -18,9 +37,23 @@ void loop() {
 }
 
 void clickLeft() {
-  
+  if (player_column > 0) {
+    player_column--;
+  }
 }
 
 void clickRight() {
-  
+  if (player_column < 4) {
+    player_column++;
+  }
+}
+
+void updateFrame() {
+  for (int i = 0; i < 5; i++) {
+    uint8_t ball;
+    uint8_t player;
+    if (i == droping_at_row) {
+      ball = ledCases[]
+    }
+  }
 }
