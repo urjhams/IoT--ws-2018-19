5fd5256316f6d49c339a77970bcf359eeaa6b5c9
Quan <urjhams@gmail.com>
2018-11-07 08:59:08 +0100

    Update sketch_homework_17_10/sketch_homework_17_10.ino

--- sketch_homework_17_10/sketch_homework_17_10.ino
+++ sketch_homework_17_10/sketch_homework_17_10.ino
@@ -10,6 +10,18 @@ int player_column = 2;
 // the ball at first start to drop at row 1 (index 0)
 int droping_at_row = 0;
 
+// LED matrix
+uint8_t matrix[5];
+
+// speed (milisecond)
+int spd = 300;
+
+// number of winning rounds
+int wins = 0;
+
+// current round
+int rounds = 1;
+
 enum cases {
   empty = B00000,
   first = B10000,
@@ -21,6 +33,20 @@ enum cases {
 
 const uint8_t ledCases[] = {first, second, third, fourth, fifth};
 
+const uint8_t win[] = 
+  { B00100,
+    B01010,
+    B10001,
+    B01010,
+    B00100, };
+
+const uint8_t lose[] =
+  { B10001,
+    B01010,
+    B00100,
+    B01010,
+    B10001, };
+    
 OneButton buttonLeft(5, true);
 OneButton buttonRight(11, true);
 
@@ -33,7 +59,14 @@ void setup() {
 void loop() {
   buttonLeft.tick();
   buttonRight.tick();
-  delay(10);
+  microbit.show(matrix);
+  delay(spd);
+
+  if (droping_at_row == 4) {
+    endRound();
+  } else {
+    droping_at_row++;
+  }
 }
 
 void clickLeft() {
@@ -48,12 +81,40 @@ void clickRight() {
   }
 }
 
+void endRound() {
+  if (drop_column == player_column) {  
+    wins++; 
+    microbit.show(win); 
+  } else {       
+    microbit.show(lose); 
+  }
+
+  if (rounds == 5) {
+    endGame();
+  } else {
+    rounds++;
+    droping_at_row = 0;
+    player_column = 2;
+    drop_column = random(5);
+    delay(1000);
+  }
+  
+}
+
+void endGame() {
+  rounds = 1;
+  microbit.print(wins);
+  wins = 0;
+  droping_at_row = 0;
+  delay(2000);
+}
+
 void updateFrame() {
   for (int i = 0; i < 5; i++) {
     uint8_t ball;
     uint8_t player;
-    if (i == droping_at_row) {
-      ball = ledCases[]
-    }
+    ball = (i == droping_at_row) ? ledCases[drop_column] : empty;
+    player = (i == 4) ? ledCases[player_column] : empty;
+    matrix[i] = ball | player;
   }
 }
